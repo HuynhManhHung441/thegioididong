709bb8461a17cbb030bc31abca1f83c60694e33d
HuynhManhHung441 <huynhmanhhung441@gmail.com>
2023-10-19 22:10:02 +0700

    show/hidden accordion of other information list on tablet & mobile

--- static/js/index/footer.js
+++ static/js/index/footer.js
@@ -1,20 +1,8 @@
-// let otherInformation = document.querySelector("information-discount--on-tablet-mobile .other-information")
 
-// let otherInformationList = document.querySelector(".other-information ul")
-// console.log("LIST",otherInformationList);
-
-// let otherInformationListOnOffSign = document.querySelector(".other-information p i")
-// console.log("Sign", otherInformationListOnOffSign)
-
-// otherInformationListOnOffSign.onclick = function() {
-//     otherInformationList.style.display = "none";
-//     otherInformation.style.clientHeight = "50px";
-//     console.log("height", otherInformation.clientHeight)
-// };
-
-const otherInformationElement = document.querySelector('.other-information ul');
-const sortDownIconElement = document.querySelector(".other-information p i");
-
-// sortDownIconElement.addEventListener('click', () => {
-//   otherInformationElement.classList.toggle('active');
-// });
+const otherInformationElement = document.querySelector('.other-information');
+const sortDownIconElement = document.querySelector(".other-information p");
+console.log(otherInformationElement)
+console.log(sortDownIconElement)
+sortDownIconElement.addEventListener('click', () => {
+  otherInformationElement.classList.toggle('active');
+});
