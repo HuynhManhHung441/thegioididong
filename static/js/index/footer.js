51643f6289bdc8f761e63ed65746794632aeef13
HuynhManhHung441 <huynhmanhhung441@gmail.com>
2023-10-20 17:09:42 +0700

    extend policy list in footer__contents when click see more text

--- static/js/index/footer.js
+++ static/js/index/footer.js
@@ -1,3 +1,13 @@
+const seeMorePolicyInFooter = document.querySelector('.footer__contents .policy');
+const sortDownIconPolicy = document.querySelector(".footer__contents .policy p");
+
+sortDownIconPolicy.addEventListener('click', () => {
+  seeMorePolicyInFooter.classList.toggle('active');
+  sortDownIconPolicy.remove();
+//   sortDownIconPolicy.setAttribute("display: none;")
+});
+
+
 
 const otherInformationElement = document.querySelector('.other-information');
 const sortDownIconElement = document.querySelector(".other-information p");
