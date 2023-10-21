fc8b65854c91b3c93ce9769051533e0350f71c8a
HuynhManhHung441 <huynhmanhhung441@gmail.com>
2023-10-21 22:07:17 +0700

    update all-cellphone page on laptop

--- static/js/index/footer.js
+++ static/js/index/footer.js
@@ -4,9 +4,7 @@ const sortDownIconPolicy = document.querySelector(".footer__contents .policy p")
 sortDownIconPolicy.addEventListener('click', () => {
   seeMorePolicyInFooter.classList.toggle('active');
   sortDownIconPolicy.remove();
-//   sortDownIconPolicy.setAttribute("display: none;")
 });
-
 
 
 const otherInformationElement = document.querySelector('.other-information');
