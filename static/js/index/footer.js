33fadfc21752406368be4b6eec159cd3d7fa73dc
HuynhManhHung441 <huynhmanhhung441@gmail.com>
2023-10-22 17:19:39 +0700

    Revert "Revert "extend policy list in footer__contents when click see more text""
    This reverts commit eb3b72ba16647778682b037b5902da4cfc78b58f.

--- static/js/index/footer.js
+++ static/js/index/footer.js
@@ -4,7 +4,9 @@ const sortDownIconPolicy = document.querySelector(".footer__contents .policy p")
 sortDownIconPolicy.addEventListener('click', () => {
   seeMorePolicyInFooter.classList.toggle('active');
   sortDownIconPolicy.remove();
+//   sortDownIconPolicy.setAttribute("display: none;")
 });
+
 
 
 const otherInformationElement = document.querySelector('.other-information');
