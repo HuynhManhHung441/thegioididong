748bddeda72d5627172a64422b2415e41a6d5814
HuynhManhHung441 <huynhmanhhung441@gmail.com>
2023-11-02 18:26:05 +0700

    add content into total-filter-modal in all-cellphones page

--- static/js/total-filter-modal.js
+++ static/js/total-filter-modal.js
@@ -7,7 +7,7 @@ var btn = document.getElementById("totalFilterBtn");
 console.log(btn)
 
 // Get the <span> element that closes the modal
-// var span = document.getElementsByClassName("close")[0];
+var closeModalBtn = document.querySelector(".close-modal");
 
 // When the user clicks the button, open the modal 
 btn.onclick = function() {
@@ -17,9 +17,9 @@ btn.onclick = function() {
 }
 
 // When the user clicks on <span> (x), close the modal
-// span.onclick = function() {
-//   modal.style.display = "none";
-// }
+closeModalBtn.onclick = function() {
+  modal.style.display = "none";
+}
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
