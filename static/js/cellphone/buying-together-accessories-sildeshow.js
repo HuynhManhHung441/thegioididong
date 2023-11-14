87741850163aba6aca52fd62e47e728ff4441d23
HuynhManhHung441 <huynhmanhhung441@gmail.com>
2023-11-14 22:32:33 +0700

    style buying-together-accessories-slideshow to control it in any-cellphone page on laptop

---
+++ static/js/cellphone/buying-together-accessories-sildeshow.js
@@ -0,0 +1,4 @@
+const buyingTogetherAccessoriesSlideshow = document.querySelector(".buying-together-accessories__sildeshow")
+const buyingTogetherAccessoriesList = document.querySelector(".buying-together-accessories__list")
+
+console.log(buyingTogetherAccessoriesList)
