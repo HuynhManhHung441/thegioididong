3ace744ce383755961e7728eee318ce4350bdf26
HuynhManhHung441 <huynhmanhhung441@gmail.com>
2023-11-16 17:27:05 +0700

    control buying-together-accessories-slideshow by click into btns in any-cellphone page on laptop

--- static/js/cellphone/buying-together-accessories-sildeshow.js
+++ static/js/cellphone/buying-together-accessories-sildeshow.js
@@ -1,4 +1,23 @@
 const buyingTogetherAccessoriesSlideshow = document.querySelector(".buying-together-accessories__sildeshow")
 const buyingTogetherAccessoriesList = document.querySelector(".buying-together-accessories__list")
 
-console.log(buyingTogetherAccessoriesList)
+const slideButtons = document.querySelector(".slide-buttons")
+const nextBtn = document.querySelector(".slide-buttons .slide-buttons__next-btn")
+const prevBtn = document.querySelector(".slide-buttons .slide-buttons__prev-btn")
+function moveNextSlide() {
+    nextBtn.style.display = "none";
+    slideButtons.style.justifyContent = "start";
+    prevBtn.style.display = "block";
+    buyingTogetherAccessoriesList.style.transform = `translateX(-${buyingTogetherAccessoriesSlideshow.clientWidth}px)`;
+}
+
+function movePrevSlide() {
+    prevBtn.style.display = "none";
+    slideButtons.style.justifyContent = "end";
+    nextBtn.style.display = "block";
+    buyingTogetherAccessoriesList.style.transform = `translateX(${0}px)`;
+}
+
+
+
+
