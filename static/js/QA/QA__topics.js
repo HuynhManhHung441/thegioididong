57d426fcfd1c0763d61811242938bf2f7afb1982
HuynhManhHung441 <huynhmanhhung441@gmail.com>
2023-12-15 14:53:35 +0700

    show/hide sub level 2 topics in QA page on laptop

--- static/js/QA/QA__topics.js
+++ static/js/QA/QA__topics.js
@@ -1,1 +1,17 @@
-const QAcontent = document.querySelector(".QA__content")
+const QAtopic = document.querySelector(".QA__topics")
+const topicList = QAtopic.querySelector("ul")
+const topicArray = topicList.querySelectorAll(".QA__specific-topic");
+
+topicArray.forEach(function(specificTopic) {
+    let TopicTitleFrame = specificTopic.querySelector("div");
+    TopicTitleFrame.addEventListener("click", () => {
+        console.log(specificTopic.querySelector("ul"));
+        let subtopics = specificTopic.querySelector("ul");  
+        if (subtopics.style.display === "none" || subtopics.style.display === "") {
+            subtopics.style.display = "flex";
+        } else {
+            subtopics.style.display = "none";
+        }
+    })
+})
+
