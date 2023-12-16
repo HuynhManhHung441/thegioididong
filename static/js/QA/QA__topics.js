4ef1105265b62864d30fac182734d85b9b93c1e5
HuynhManhHung441 <huynhmanhhung441@gmail.com>
2023-12-16 16:42:46 +0700

    hide and show QA-articles in QA page on laptop

--- static/js/QA/QA__topics.js
+++ static/js/QA/QA__topics.js
@@ -15,3 +15,65 @@ topicArray.forEach(function(specificTopic) {
     })
 })
 
+
+const qaArticle = document.querySelector(".QA__articles")
+console.log(qaArticle)
+// const lastAppearArticle = document.querySelector("#lastAppearArticle")
+
+const qaArticlesItems = qaArticle.querySelector(".QA-articles__items")
+
+let qaArticleItemList = qaArticlesItems.querySelectorAll(".QA-article-item")
+console.log(qaArticleItemList)
+
+const seeMoreQaArticleBtn = qaArticle.querySelector("#seeMoreQaArticleBtn")
+// qaArticleItemList.forEach(function(qaArticleItem) {
+//     qaArticleItem
+// })
+let indexQaArticleItem = 0;
+let numberItemAppear = 10;
+let appearStep = 5;
+
+
+function appearItems(numberItemAppear) {
+    for (let i = 0; i < qaArticleItemList.length; i++) {
+        // console.log(qaArticleItemList[i])
+        // if (qaArticleItemList[i].getAttribute('id') === "lastAppearArticleAtFirstTime"){
+        if ((i+1) <= numberItemAppear){
+            qaArticleItemList[i].classList.remove("hide");
+        } else {
+            qaArticleItemList[i].classList.add("hide");
+        }
+        // if (i === qaArticleItemList.length -1){
+        //     if (!qaArticleItemList[i].classList.contains("hide")){
+        //         seeMoreQaArticleBtn.classList.add("hide")
+        //     }
+        // }
+    }
+    if (numberItemAppear >= qaArticleItemList.length) {
+        seeMoreQaArticleBtn.classList.add("hide")
+    }
+}
+
+appearItems(numberItemAppear)
+seeMoreQaArticleBtn.addEventListener("click", () => {
+    numberItemAppear += appearStep;
+    appearItems(numberItemAppear)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+// qaArticleDivList.forEach(function(specificDiv){
+//     if
+// })
+
+// console.log(qaArticlesItems)
+
--- static/js/index/delivery-address-modal.js
+++ static/js/index/delivery-address-modal.js
@@ -3,17 +3,49 @@ const headerInventoryModalOpenBtn = document.querySelector(".main-header__invent
 const mainHeaderInventoryModal = document.querySelector(".delivery-address__modal-overlay");
 const headerInventoryModalCloseBtn = document.querySelector(".delivery-address__close-modal-btn");
  
-function openModal() {
-    mainHeaderInventoryModal.classList.remove("hide");
+function handleOpenCloseModal () {
+    function openModal() {
+        mainHeaderInventoryModal.classList.remove("hide");
+    }
+     
+    function closeModal(e, clickedOutside) {
+        if (clickedOutside) {
+            if (e.target.classList.contains("delivery-address__modal-overlay"))
+            mainHeaderInventoryModal.classList.add("hide");
+        } else mainHeaderInventoryModal.classList.add("hide");
+    }
+     
+    headerInventoryModalOpenBtn.addEventListener("click", openModal);
+    mainHeaderInventoryModal.addEventListener("click", (e) => closeModal(e, true));
+    headerInventoryModalCloseBtn.addEventListener("click", closeModal);
 }
- 
-function closeModal(e, clickedOutside) {
-    if (clickedOutside) {
-        if (e.target.classList.contains("delivery-address__modal-overlay"))
-        mainHeaderInventoryModal.classList.add("hide");
-    } else mainHeaderInventoryModal.classList.add("hide");
+
+async function  getLocationData (){
+    const response = await fetch('https://provinces.open-api.vn/api/p/')
+    const results = await response.json()
+
+
+    return results
 }
- 
-headerInventoryModalOpenBtn.addEventListener("click", openModal);
-mainHeaderInventoryModal.addEventListener("click", (e) => closeModal(e, true));
-headerInventoryModalCloseBtn.addEventListener("click", closeModal);
+
+function renderModalContent(){
+    const deliveryAddressProvinceList = document.querySelector(".delivery-address__province-list");
+
+    getLocationData()
+    .then(results => {
+        console.log(results)
+        let html = ''
+        for(let province of results) {
+            html += `<li>${province.name}</li>`
+        }
+        deliveryAddressProvinceList.innerHTML = html
+    })
+    .catch(e => {
+        console.log(e)
+    })
+}
+
+
+renderModalContent()
+handleOpenCloseModal();
+
